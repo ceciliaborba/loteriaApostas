3770a63d5a98a46c0ec1087d9d0d08d6917d97dc
André Pacheco <anddreluiz1@gmail.com>
2017-05-12 16:33:53 -0300

    Update loteria.php

--- loteria.php
+++ loteria.php
@@ -1,44 +1,44 @@
 <?php
-include 'sorteador.php';
 
-menu();
+    include 'geradorApostas.php';
+    menu();
 
-function menu()
-{
-    echo "Digite 1 para apostar na MegaSena \n";
-    echo "Digite 2 para apostar na Quina \n";
-    echo "Digite 3 para apostar na LotoMania \n";
-    echo "Digite 4 para apostar na Lotofacil \n";
-    echo "Digite 5 para sair \n";
+    function menu(){
+        echo "Digite 1 para apostar na MegaSena \n";
+        echo "Digite 2 para apostar na Quina \n";
+        echo "Digite 3 para apostar na LotoMania \n";
+        echo "Digite 4 para apostar na Lotofacil \n";
+        echo "Digite 5 para sair \n";
 
-    $alternativa = fgets(STDIN);
+        $alternativa = fgets(STDIN);
 
-    switch ($alternativa) {
+        switch ($alternativa) {
 
-        case 1:
-            geradorApostas(60, 6, 15);
-            menu();
-            break;
+            case 1:
+                geradorApostas(60, 6, 15);
 
-        case 2:
-            geradorApostas(80, 5, 15);
-            break;
+                break;
 
-        case 3:
-            geradorApostas(100, 50, 50);
-            break;
+            case 2:
+                geradorApostas(80, 5, 15);
+                break;
 
-        case 4:
-            geradorApostas(25, 15, 18);
-            break;
+            case 3:
+                geradorApostas(100, 50, 50);
+                break;
 
-        case 5:
-            exit();
-            break;
+            case 4:
+                geradorApostas(25, 15, 18);
+                break;
 
-        default:
-            echo "numero invalido";
-            menu();
-            break;
+            case 5:
+                exit();
+                break;
+
+            default:
+                echo "numero invalido";
+                menu();
+                break;
+        }
     }
-}
+
