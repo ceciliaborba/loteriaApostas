99dc8e6f50ed7ee87983408670d13e6c17504c00
André Pacheco <anddreluiz1@gmail.com>
2017-05-18 10:21:57 -0300

    Update GeradorApostas.php

--- GeradorApostas.php
+++ GeradorApostas.php
@@ -1,141 +1,239 @@
 <?php
-    
-    function geradorApostas($possibilidades, $min, $max){
-    
+
+$alternativa = 0;
+
+function geradorApostasMega($possibilidades, $min, $max){
+
+    global $alternativa;
+
+    echo "Quantas dezenas você deseja sortear?";
+
+    $qtdeDezenas = trim(fgets(STDIN));
+
+    while ($qtdeDezenas < $min || $qtdeDezenas > $max) {
+
+        echo "Inválido. Quantas dezenas você deseja sortear";
+        $qtdeDezenas = fgets(STDIN);
+    }
+    echo "Quantos jogos você quer apostar?";
+    $qtdeApostas = trim(fgets(STDIN));
+
+    for($i = 0; $i < $qtdeApostas; $i++){
+        $dezenasSorteadas = [];
+        for($j = 0; $j < $qtdeDezenas; $j++){
+            $num = rand(1, $possibilidades);
+            if(!in_array($num, $dezenasSorteadas)){
+                $dezenasSorteadas[$j] = $num;
+            }else{
+                $j--;
+            }
+        }
+        echo "resultado aposta ".($i+1).": ";
+        sort($dezenasSorteadas);
+        foreach ($dezenasSorteadas as $dezena){
+            echo "$dezena ";
+        }
+        echo "\n";
+    }
+    echo "\n";
+
+    if ($alternativa == 1 && $qtdeDezenas == 6) {
+        $pagar = $qtdeApostas * 3.50;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 1 && $qtdeDezenas == 7) {
+        $pagar = $qtdeApostas * 24.50;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 1 && $qtdeDezenas == 8) {
+        $pagar = $qtdeApostas * 98;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 1 && $qtdeDezenas == 9) {
+        $pagar = $qtdeApostas * 294;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 1 && $qtdeDezenas == 10) {
+        $pagar = $qtdeApostas * 735;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 1 && $qtdeDezenas == 11) {
+        $pagar = $qtdeApostas * 1617;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 1 && $qtdeDezenas == 12) {
+        $pagar = $qtdeApostas * 3234;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 1 && $qtdeDezenas == 13) {
+        $pagar = $qtdeApostas * 6006;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 1 && $qtdeDezenas == 14) {
+        $pagar = $qtdeApostas * 10510.50;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 1 && $qtdeDezenas == 15) {
+        $pagar = $qtdeApostas * 17517.50;
+        echo "$pagar reais \n";
+    } else {
+        echo "Numero invalido \n";
+    }
+}
+
+function geradorApostasQuina($possibilidades, $min, $max)
+{
+
+    global $alternativa;
+
+    echo "Quantas dezenas você deseja sortear?";
+
+    $qtdeDezenas = trim(fgets(STDIN));
+
+    while ($qtdeDezenas < $min || $qtdeDezenas > $max) {
+
+        echo "Inválido. Quantas dezenas você deseja sortear";
+        $qtdeDezenas = fgets(STDIN);
+    }
+    echo "Quantos jogos você quer apostar?";
+    $qtdeApostas = trim(fgets(STDIN));
+    for ($i = 0; $i < $qtdeApostas; $i++) {
+        $dezenasSorteadas = [];
+        for ($j = 0; $j < $qtdeDezenas; $j++) {
+            $num = rand(1, $possibilidades);
+            if (!in_array($num, $dezenasSorteadas)) {
+                $dezenasSorteadas[$j] = $num;
+            } else {
+                $j--;
+            }
+        }
+        echo "resultado aposta " . ($i + 1) . ": ";
+        sort($dezenasSorteadas);
+        foreach ($dezenasSorteadas as $dezena) {
+            echo "$dezena ";
+        }
+        echo "\n";
+    }
+    echo "\n";
+
+    if ($alternativa == 2 && $qtdeDezenas == 5) {
+        $pagar = $qtdeApostas * 1.50;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 2 && $qtdeDezenas == 6) {
+        $pagar = $qtdeApostas * 9;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 2 && $qtdeDezenas == 7) {
+        $pagar = $qtdeApostas * 31.50;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 2 && $qtdeDezenas == 8) {
+        $pagar = $qtdeApostas * 84;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 2 && $qtdeDezenas == 9) {
+        $pagar = $qtdeApostas * 189;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 2 && $qtdeDezenas == 10) {
+        $pagar = $qtdeApostas * 378;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 2 && $qtdeDezenas == 11) {
+        $pagar = $qtdeApostas * 693;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 2 && $qtdeDezenas == 12) {
+        $pagar = $qtdeApostas * 1188;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 2 && $qtdeDezenas == 13) {
+        $pagar = $qtdeApostas * 1930.50;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 2 && $qtdeDezenas == 14) {
+        $pagar = $qtdeApostas * 3003;
+        echo "$pagar reais \n";
+    } elseif ($alternativa == 2 && $qtdeDezenas == 15) {
+        $pagar = $qtdeApostas * 4504.50;
+        echo "$pagar reais \n";
+    } else {
+        echo "Numero invalido";
+    }
+}
+
+    function geradorApostasMania($possibilidades, $min, $max)
+    {
+
+        global $alternativa;
+
         echo "Quantas dezenas você deseja sortear?";
-    
+
         $qtdeDezenas = trim(fgets(STDIN));
-    
+
         while ($qtdeDezenas < $min || $qtdeDezenas > $max) {
-    
+
             echo "Inválido. Quantas dezenas você deseja sortear";
             $qtdeDezenas = fgets(STDIN);
         }
-
         echo "Quantos jogos você quer apostar?";
         $qtdeApostas = trim(fgets(STDIN));
-
-        for($i = 0; $i < $qtdeApostas; $i++){
+        for ($i = 0; $i < $qtdeApostas; $i++) {
             $dezenasSorteadas = [];
-
-            for($j = 0; $j < $qtdeDezenas; $j++){
+            for ($j = 0; $j < $qtdeDezenas; $j++) {
                 $num = rand(1, $possibilidades);
-
-                if(!in_array($num, $dezenasSorteadas)){
+                if (!in_array($num, $dezenasSorteadas)) {
                     $dezenasSorteadas[$j] = $num;
-                }else{
+                } else {
                     $j--;
                 }
             }
-
-            echo "resultado aposta ".($i+1).": ";
-
+            echo "resultado aposta " . ($i + 1) . ": ";
             sort($dezenasSorteadas);
-
-            foreach ($dezenasSorteadas as $dezena){
+            foreach ($dezenasSorteadas as $dezena) {
                 echo "$dezena ";
             }
             echo "\n";
         }
         echo "\n";
+
+
+        if ($alternativa == 3 && $qtdeDezenas == 50) {
+            $pagar = $qtdeApostas * 1.50;
+            echo "$pagar reais \n";
+        } else {
+            echo "Numero invalido";
+        }
     }
 
-    //--------------------------------------MEGA SENA----------------------------------
+        function geradorApostasFacil($possibilidades, $min, $max)
+        {
 
-    if ($alternativa == 1 && $qtdeDezenas == 6) {
-        $pagar = $qtdeDezenas * 3.50;
-        echo $pagar;
-    } elseif ($alternativa == 1 && $qtdeDezenas == 7) {
-        $pagar = $qtdeDezenas * 24.50;
-        echo $pagar;
-    } elseif ($alternativa == 1 && $qtdeDezenas == 8) {
-        $pagar = $qtdeDezenas * 98;
-        echo $pagar;
-    } elseif ($alternativa == 1 && $qtdeDezenas == 9) {
-        $pagar = $qtdeDezenas * 294;
-        echo $pagar;
-    } elseif ($alternativa == 1 && $qtdeDezenas == 10) {
-        $pagar = $qtdeDezenas * 735;
-        echo $pagar;
-    } elseif ($alternativa == 1 && $qtdeDezenas == 11) {
-        $pagar = $qtdeDezenas * 1617;
-        echo $pagar;
-    } elseif ($alternativa == 1 && $qtdeDezenas == 12) {
-        $pagar = $qtdeDezenas * 3234;
-        echo $pagar;
-    } elseif ($alternativa == 1 && $qtdeDezenas == 13) {
-        $pagar = $qtdeDezenas * 6006;
-        echo $pagar;
-    } elseif ($alternativa == 1 && $qtdeDezenas == 14) {
-        $pagar = $qtdeDezenas * 10510.50;
-        echo $pagar;
-    } elseif ($alternativa == 1 && $qtdeDezenas == 15) {
-        $pagar = $qtdeDezenas * 17517.50;
-        echo $pagar;
-    } else {
-        echo "Numero invalido";
-    }
+            global $alternativa;
 
-    //-----------------------------------QUINA-----------------------------------
+            echo "Quantas dezenas você deseja sortear?";
 
-    if ($alternativa == 2 && $qtdeDezenas == 5) {
-        $pagar = $qtdeDezenas * 1.50;
-        echo $pagar;
-    } elseif ($alternativa == 2 && $qtdeDezenas == 6) {
-        $pagar = $qtdeDezenas * 9;
-        echo $pagar;
-    } elseif ($alternativa == 2 && $qtdeDezenas == 7) {
-        $pagar = $qtdeDezenas * 31.50;
-        echo $pagar;
-    } elseif ($alternativa == 2 && $qtdeDezenas == 8) {
-        $pagar = $qtdeDezenas * 84;
-        echo $pagar;
-    } elseif ($alternativa == 2 && $qtdeDezenas == 9) {
-        $pagar = $qtdeDezenas * 189;
-        echo $pagar;
-    } elseif ($alternativa == 2 && $qtdeDezenas == 10) {
-        $pagar = $qtdeDezenas * 378;
-        echo $pagar;
-    } elseif ($alternativa == 2 && $qtdeDezenas == 11) {
-        $pagar = $qtdeDezenas * 693;
-        echo $pagar;
-    } elseif ($alternativa == 2 && $qtdeDezenas == 12) {
-        $pagar = $qtdeDezenas * 1188;
-        echo $pagar;
-    } elseif ($alternativa == 2 && $qtdeDezenas == 13) {
-        $pagar = $qtdeDezenas * 1930.50;
-        echo $pagar;
-    } elseif ($alternativa == 2 && $qtdeDezenas == 14) {
-        $pagar = $qtdeDezenas * 3003;
-        echo $pagar;
-    } elseif ($alternativa == 2 && $qtdeDezenas == 15) {
-        $pagar = $qtdeDezenas * 4504.50;
-        echo $pagar;
-    } else {
-        echo "Numero invalido";
-    }
+            $qtdeDezenas = trim(fgets(STDIN));
 
-    //-----------------------------------LOTO MANIA---------------------------
+            while ($qtdeDezenas < $min || $qtdeDezenas > $max) {
 
-    if ($alternativa == 3 && $qtdeDezenas == 50) {
-        $pagar = $qtdeDezenas * 1.50;
-        echo "$pagar";
-    } else {
-        echo "Numero invalido";
-    }
-
-    //-----------------------------------LOTO FACIL------------------------------
-
-    if($alternativa == 4 && $qtdeDezenas == 15){
-        $pagar = $qtdeDezenas * 2;
-        echo $pagar;
-    } elseif ($alternativa == 4 && $qtdeDezenas == 16) {
-        $pagar = $qtdeDezenas * 32;
-        echo $pagar;
-    } elseif ($alternativa == 4 && $qtdeDezenas == 17) {
-        $pagar = $qtdeDezenas * 272;
-        echo $pagar;
-    } elseif ($alternativa == 4 && $qtdeDezenas == 18) {
-        $pagar = $qtdeDezenas * 1632;
-        echo $pagar;
-    }
+                echo "Inválido. Quantas dezenas você deseja sortear";
+                $qtdeDezenas = fgets(STDIN);
+            }
+            echo "Quantos jogos você quer apostar?";
+            $qtdeApostas = trim(fgets(STDIN));
+            for ($i = 0; $i < $qtdeApostas; $i++) {
+                $dezenasSorteadas = [];
+                for ($j = 0; $j < $qtdeDezenas; $j++) {
+                    $num = rand(1, $possibilidades);
+                    if (!in_array($num, $dezenasSorteadas)) {
+                        $dezenasSorteadas[$j] = $num;
+                    } else {
+                        $j--;
+                    }
+                }
+                echo "resultado aposta " . ($i + 1) . ": ";
+                sort($dezenasSorteadas);
+                foreach ($dezenasSorteadas as $dezena) {
+                    echo "$dezena ";
+                }
+                echo "\n";
+            }
+            echo "\n";
+            if ($alternativa == 4 && $qtdeDezenas == 15) {
+                $pagar = $qtdeApostas * 2;
+                echo "$pagar reais \n";
+            } elseif ($alternativa == 4 && $qtdeDezenas == 16) {
+                $pagar = $qtdeApostas * 32;
+                echo "$pagar reais \n";
+            } elseif ($alternativa == 4 && $qtdeDezenas == 17) {
+                $pagar = $qtdeApostas * 272;
+                echo "$pagar reais \n";
+            } elseif ($alternativa == 4 && $qtdeDezenas == 18) {
+                $pagar = $qtdeApostas * 1632;
+                echo "$pagar reais \n";
+            }
+        }
