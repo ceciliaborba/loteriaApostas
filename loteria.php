d36d6fa83eabd912d0138ef31a14a05a388d0725
André Pacheco <anddreluiz1@gmail.com>
2017-05-18 10:21:04 -0300

    Update loteria.php

--- loteria.php
+++ loteria.php
@@ -1,44 +1,45 @@
 <?php
+include 'sorteador.php';
 
-    include 'geradorApostas.php';
-    menu();
+menu();
 
-    function menu(){
-        echo "Digite 1 para apostar na MegaSena \n";
-        echo "Digite 2 para apostar na Quina \n";
-        echo "Digite 3 para apostar na LotoMania \n";
-        echo "Digite 4 para apostar na Lotofacil \n";
-        echo "Digite 5 para sair \n";
+function menu(){
+    global $alternativa;
 
-        $alternativa = fgets(STDIN);
+    echo "Digite 1 para apostar na MegaSena \n";
+    echo "Digite 2 para apostar na Quina \n";
+    echo "Digite 3 para apostar na LotoMania \n";
+    echo "Digite 4 para apostar na Lotofacil \n";
+    echo "Digite 5 para sair \n";
 
-        switch ($alternativa) {
+    $alternativa = fgets(STDIN);
 
-            case 1:
-                geradorApostas(60, 6, 15);
+    switch ($alternativa) {
 
-                break;
+        case 1:
+            geradorApostasMega(60, 6, 15);
+            menu();
+            break;
 
-            case 2:
-                geradorApostas(80, 5, 15);
-                break;
+        case 2:
+            geradorApostasQuina(80, 5, 15);
+            break;
 
-            case 3:
-                geradorApostas(100, 50, 50);
-                break;
+        case 3:
+            geradorApostasMania(100, 50, 50);
+            break;
 
-            case 4:
-                geradorApostas(25, 15, 18);
-                break;
+        case 4:
+            geradorApostasFacil(25, 15, 18);
+            break;
 
-            case 5:
-                exit();
-                break;
+        case 5:
+            exit();
+            break;
 
-            default:
-                echo "numero invalido";
-                menu();
-                break;
-        }
+        default:
+            echo "numero invalido";
+            menu();
+            break;
     }
-
+}
